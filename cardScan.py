ec0d4a415d1a6112626d102e029f4df6b49a3167
Bobcati <108192537+Bobcati@users.noreply.github.com>
2023-06-12 14:04:55 +0100

    Add files via upload
    Added a manual fixer option

--- cardScan.py
+++ cardScan.py
@@ -31,6 +31,7 @@ picSet = int(input("Enter picture set number: ")) # Global variable for the hard
 cardPicList = []
 cardList = []
 cardIndex = 0 # must be global otherwise resets to 0 every time, which is bad!
+imagesTaken = 0
 
 for i in range(30):
     cardPicList.append("card_" + str((i + 1)) + ".jpg")
@@ -39,9 +40,11 @@ for i in range(30):
 # Initialize the camera for first image processing and take picture
 def takePicture():
     global cardPicList
+    global imagesTaken
     for i in range(30):
         take = input("Take a photo (y/n): ")
         if (take.lower() != "n"):
+            imagesTaken = imagesTaken + 1
             picam2.capture_file("Card_Pics_" + str(picSet) + "/" + cardPicList[i]) # Saves images to dedicated image directory for each hard drive log
             print("Image " + str(i+1) + " captured")
 
@@ -66,13 +69,29 @@ def textImageProcessor():
 
     # hdImage = cv2.cvtColor(hdImage, cv2.COLOR_BGR2GRAY) # Convert to greyscale for better processing - not working right now
     cardIndex = cardIndex + 1 # Prepares for next loop
-    
-
     rawText = pytesseract.image_to_data(croppedImage, output_type=Output.DICT) # Raw dictionary 
     recordedList = rawText['text'] #Text list subset of rawText diciontary
     recordedString = ' '.join(rawText['text'])
     print("Recorded String \n" + recordedString)
     cardList.append(recordedString)
+
+def fixCardDataAndAdd():
+    for i in range(30):
+        print("Recorded Card Serial Numbers: \n")
+        print("Card #" + str(i+1) + ": " + cardList[i] + "\n")
+
+    needToFix = (input("Do you want to manually fix serial numbers? (Y/N)"))
+    while needToFix.upper() != "Y" or needToFix.upper() != "N":
+        if needToFix.upper() == "Y": # NEED TO  ADD INCORRECT INPUT REJECTION ie. non integer inputs, 0 entered by accident
+            cardNumToFix = int(input("Enter the number you want to fix (ie. 1, 3, 20): "))
+            print("You have chosen to fix card #" + str(cardNumToFix) + ": " + cardList[cardNumToFix - 1])
+            correctNum = input("Enter the corrected serial number: ")
+            cardList[cardNumToFix - 1] = correctNum
+        elif needToFix.upper() == "N":
+            break
+        else:
+            print("Please type a valid answer")
+        needToFix = (input("Do you want to manually fix serial numbers? (Y/N)"))
 
 #Function that writes the HDD report
 def writeReport():
@@ -89,10 +108,10 @@ def writeReport():
         takePicture()
         for i in range(30):
             textImageProcessor()
-        print(str(cardList))
-        csvScribe()
-        print("Report #" + str(picSet) + " completed")
-        time.sleep(3)
+    fixCardDataAndAdd()
+    csvScribe()
+    print("Report #" + str(picSet) + " completed")
+    time.sleep(3)
 
 # Might be redundanct (cvsCreator already does the job)
 def header():
@@ -125,6 +144,7 @@ def csvScribe():
 def looper():
     global picSet
     global cardIndex
+    global imagesTaken
     header()
     writeReport()
 
@@ -133,6 +153,7 @@ def looper():
         if writeAnother.upper() == "Y":
             picSet = int(input("Enter picture set number: "))
             cardIndex = 0
+            imagesTaken = 0
             looper()
         elif writeAnother.upper() == "N":
             print("Goodbye: ")
